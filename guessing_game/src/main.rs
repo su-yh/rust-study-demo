4189bf256fdfb3b18e6be448b9f036201308c367
suyh <su_787910081@sina.com>
2023-05-01 15:20:13 +0800

    1

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,42 +1,34 @@
-// prelude
-use std::cmp::Ordering;
-use rand::Rng; // trait
+// 为什么必须要引入它，在不引入它的时候会报错？
+use rand::Rng;
 
 fn main() {
     println!("猜数！");
 
-    // i32  u32  i64
     let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    println!("神秘数字是: {}", secret_number);
+    println!("神秘数字是：{}", secret_number);
 
     loop {
-        println!("猜测一个数");
+        println!("猜测一个数！");
 
-        // 使用mut 标识该变量是可变的。否则不可变
         let mut guess = String::new();
 
-        std::io::stdin().read_line(&mut guess)
-        // .expect 处理如果读取发生错误的问题。
-        .expect("无法读取行");
-        // io::Result Ok, Err
+        // io::Result Ok, Err 两个变体
+        let res = std::io::stdin().read_line(&mut guess).expect("读取失败！");
 
-        // 定义一个u32 类型的变量
-        // rust 允许 同名的变量，将前面的同名的变量隐藏。
-        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
-        let guess: u32 = match guess.trim().parse()
-        {
+        println!("您的输入是：{}", guess);
+        println!("返回值结果: {}", res);
+
+        // shadow 隐藏同名的变量，一般用在类型变化
+        // let guess: i32 = guess.trim().parse().expect("解析失败，你的输入 不是数字");
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
-        } ;
-
-
-        println!("你猜测的数是：{}", guess);
+        };
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
+            std::cmp::Ordering::Less => println!("Too small!"),
+            std::cmp::Ordering::Greater => println!("Too big"),
+            std::cmp::Ordering::Equal => {
                 println!("You win!");
                 break;
             },
